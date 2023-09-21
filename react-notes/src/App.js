f63f337f1c6449f87f2a3cb0d46bab11fe5974b0
Max Petzold <maxpetzold581@gmail.com>
2023-09-21 12:38:24 +0200

    Lagde notesForm component

---
+++ react-notes/src/App.js
@@ -0,0 +1,13 @@
+import NotesForm from "./components/notesform";
+
+function App() {
+    return (
+        <div className={"flex flex-col items-center w-screen h-screen bg-cyan-950 text-white"}>
+            <NotesForm/>
+        </div>
+
+
+    );
+}
+
+export default App;
