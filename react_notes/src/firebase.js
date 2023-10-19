ecfcbd005dc590927f471436d78210a8591728b1
Max Petzold <maxpetzold581@gmail.com>
2023-10-19 10:28:31 +0200

    la til firebase
    skal lage backend med firebase

---
+++ react_notes/src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcAMYE_Ekc1dqKwZWHepMjVwoxmSZPojE",
+  authDomain: "react-notes-project-8e174.firebaseapp.com",
+  projectId: "react-notes-project-8e174",
+  storageBucket: "react-notes-project-8e174.appspot.com",
+  messagingSenderId: "235279811987",
+  appId: "1:235279811987:web:20a2c921ee0d4f2361d466",
+  measurementId: "G-SYPSWHYD9B",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const firestore = getFirestore(app);
