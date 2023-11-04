61a60cfa4d83412b827ceb143fb3791746233b2c
Max Petzold <maxpetzold581@gmail.com>
2023-11-04 12:38:40 +0100

    component work
    må pushe mye pga pc bytting

--- react_notes/src/components/about/about.jsx
+++ react_notes/src/components/about/about.jsx
@@ -5,8 +5,8 @@ export function About({ clicked, setaboutClicked }) {
   const [Display, setDisplay] = useState(true);
 
   return (
-    <div className="absolute w-1/2 h-full top-2 flex flex-col  justify-center ">
-      <div className="flex flex-col h-2/3 bg-slate-800 rounded-md ">
+    <div className="absolute w-1/2 h-fit bg-red-400 top-2 flex flex-col  justify-center ">
+      <div className="flex flex-col h-2/3 bg-slate-800 w-full ">
         <button
           onClick={() => setaboutClicked(false)}
           className="hover:text-green-300 w-fit h-fit flex justify-start text-xl p-5 items-center hover:border-s-2 border-green-400 "
@@ -14,22 +14,36 @@ export function About({ clicked, setaboutClicked }) {
           <AiOutlineClose />
         </button>
 
-        <section className="flex flex-col justify-center text-md md:text-xl lg:text-xl  w-fit gap-5 p-8">
+        <section className="flex flex-col justify-center text-md md:text-xl lg:text-xl  w-full gap-5 p-8">
           {" "}
           {/* tror egt ikke section taggen er nødvendig, men nå er den her:)*/}
           <h1 className="justify-center text-3xl md:text-4xl lg:text-6xl bold w-full ">
             About us
           </h1>
-          <p className="">
-            This is a simple note taking app. By default, it stores your notes
-            in localstorage.
-          </p>
-          <div className=""></div>
-          <p>
-            If you want to use synchronization across devices you have to create
-            an account.{" "}
-          </p>
-          <div className="h-2"></div>
+          <div className="w-3/4 flex flex-col gap-3">
+            <p className="">
+              This is a simple note taking app. By default, it stores your notes
+              in localstorage.
+            </p>
+            <div className=""></div>
+            <p>
+              If you want to use synchronization across devices you have to
+              create an account.{" "}
+            </p>
+            <p>
+              Create a note by writing a title for your note, the content of
+              your note and click the save button{" "}
+            </p>
+          </div>
+          <div className="h-8"></div>
+          <div className="w-full flex justify-start ">
+            <button
+              className="rounded-md w-1/5 h-16 bg-purple-500 flex justify-center items-center "
+              onClick={() => setaboutClicked(false)}
+            >
+              Get started
+            </button>
+          </div>
         </section>
       </div>
     </div>
--- react_notes/src/pages/index.js
+++ react_notes/src/pages/index.js
@@ -1,45 +1,63 @@
-import React from "react";
+// index.js
+
+import React, { useState } from "react";
 import NotesForm from "@/components/notesform";
 import Login from "@/components/login_register/login/login";
-
-import { useState } from "react";
 import { About } from "@/components/about/about";
 
 export default function App() {
   const [loginButtonclicked, setloginButtonClicked] = useState(false);
   const [aboutClicked, setaboutClicked] = useState(false);
 
+  const handleBackdropClick = () => {
+    setaboutClicked(false);
+  };
+
   return (
     <>
       <div
         className={
-          "flex flex-col  items-center w-screen h-screen bg-primary text-white"
+          "flex flex-col items-center w-screen h-screen bg-primary text-white"
         }
       >
-        <nav className="flex flex-row  w-full h-16 justify-center items-center bg-transparent/90">
-          <button className="bg-red-400" onClick={() => setaboutClicked(true)}>
-            About
-          </button>
-
-          <div className="spacer w-4/5"></div>
-          <div className="flex flex-col">
-            <div> not signed in?</div>
+        <div className="h-full w-full bg-transparent/10">
+          <nav className="flex flex-row w-full h-16 justify-center items-center">
             <button
-              onClick={() => setloginButtonClicked(true)}
-              className="hover:text-green-400"
+              className="bg-red-400"
+              onClick={() => setaboutClicked(true)}
             >
-              log in, or create account
+              About
             </button>
-          </div>
-        </nav>
-        <div className="flex h-full w-full justify-center items-center">
-          {aboutClicked ? <About setaboutClicked={setaboutClicked} /> : null}
+
+            <div className="spacer w-4/5"></div>
+            <div className="flex flex-col">
+              <div> not signed in?</div>
+              <button
+                onClick={() => setloginButtonClicked(true)}
+                className="hover:text-green-400"
+              >
+                log in, or create account
+              </button>
+            </div>
+          </nav>
+          {aboutClicked && (
+            <div
+              className={`fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 ${
+                aboutClicked ? "backdrop-blur-md" : ""
+              }`}
+              onClick={handleBackdropClick}
+            ></div>
+          )}
+          {aboutClicked ? (
+            <div className="w-screen h-screen flex justify-center items-centers bg-red-300">
+              <About setaboutClicked={setaboutClicked} className="" />
+            </div>
+          ) : null}
         </div>
+        <NotesForm />
         {loginButtonclicked ? (
           <Login setloginButtonClicked={setloginButtonClicked} />
-        ) : (
-          <NotesForm />
-        )}
+        ) : null}
       </div>
     </>
   );
